e151f7975298912f65f1ca9d4e5da561a3d424ca
Nishant Kumar Jha <N1shanntjha@gmail.com>
2026-01-15 17:39:23 +0530

    minor bugs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { User, LogOut, Menu, X } from "lucide-react";
 import { useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "@/contexts/AuthContextType";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
